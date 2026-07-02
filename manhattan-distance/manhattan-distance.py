c7684e30e284a53d5dbe63260db5ed9c23acce8a
Long Le <150599487+longle0702@users.noreply.github.com>
2026-07-02 10:29:49 +0200

    Solve manhattan-distance

---
+++ manhattan-distance/manhattan-distance.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def manhattan_distance(x, y):
+    """
+    Compute the Manhattan (L1) distance between vectors x and y.
+    Must return a float.
+    """
+    x = np.array(x)
+    y = np.array(y)
+    # Write code here
+    dist = np.sum(np.abs(x - y))
+    return int(dist)
